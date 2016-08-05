199a27c91d2f32cc0d8626416dab8eb5f9aee92b
seekdestiny <jifeiqian@gmail.com>
2016-08-04 22:05:55 -0700

    basic function definition

---
+++ src/com/parkinglot/ParkingLot.java
@@ -0,0 +1,43 @@
+package com.parkinglot;
+
+import java.util.*;
+
+public class ParkingLot {
+	private Level[] levels;
+	
+	/**given a vehichle, tell me whether I can park it?**/
+	public boolean hasSpot(Vehicle v) {
+	//Todo: check each level, for each level, call Level#hasSpot(Vehicle)
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
+
+class Level {
+	//tracking Parking Spots
+	//boolean hasSpot(Vehicle);
+}
+
+class ParkingSpot {
+	//boolean fit(Vehicle): check size and availability
+	
+}
+
+enum VehicleSize {
+	Compact(1), Large(3), Regular(2);
+	
+	private final int size;
+	VehicleSize(int s) {
+		size = s;
+	}
+}
+
+abstract class Vehicle {
+	//data fields
+	//getSize() method
+}
+
+
