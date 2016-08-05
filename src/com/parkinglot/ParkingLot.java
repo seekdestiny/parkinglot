714595f70a29d31361ec6ba04f2b42e81885850c
seekdestiny <jifeiqian@gmail.com>
2016-08-04 22:40:27 -0700

    basic structure

--- src/com/parkinglot/ParkingLot.java
+++ src/com/parkinglot/ParkingLot.java
@@ -38,6 +38,22 @@ enum VehicleSize {
 abstract class Vehicle {
 	//data fields
 	//getSize() method
+	public abstract VehicleSize getSize();
 }
+
+//Car class
+class Car extends Vehicle {
+	public VehicleSize getSize() {
+		return VehicleSize.Compact;
+	}
+}
+
+//Truck class
+class Truck extends Vehicle {
+	public VehicleSize getSize() {
+		return VehicleSize.Large;
+	}
+}
+
 
 
