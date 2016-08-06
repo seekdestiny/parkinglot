131097e14276922374c2b4d7bcbb76e5b2a3e6ff
seekdestiny <jifeiqian@gmail.com>
2016-08-05 17:26:45 -0700

    define function of ParkingLot class

--- src/com/parkinglot/ParkingLot.java
+++ src/com/parkinglot/ParkingLot.java
@@ -6,11 +6,17 @@ import java.util.ArrayList;
 public class ParkingLot {
 	private Level[] levels;
 	
+	//TODO constructor method
+	
 	/**given a vehicle, tell me whether I can park it?**/
 	public boolean hasSpot(Vehicle v) {
 	//TODO: check each level, for each level, call Level#hasSpot(Vehicle)
 		return true;
 	}
+	
+	//TODO: park
+	
+	//TODO: leave
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
@@ -57,7 +63,7 @@ class ParkingSpot {
 	public boolean fit(Vehicle v) {
 		if (currentVehicle == null) {
 			// check size
-			return size.ordinal() >= v.getSize().ordinal();//TODO not using ordinal
+			return size.compareTo(v.getSize()) >= 0;
 		}
 		return false;
 	}
@@ -73,15 +79,6 @@ class ParkingSpot {
 	
 	public void leave() {
 		currentVehicle = null;
-	}
-}
-
-enum VehicleSize {
-	Compact(1), Large(3), Regular(2);
-	
-	private final int size;
-	VehicleSize(int s) {
-		size = s;
 	}
 }
 
